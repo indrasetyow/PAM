b94dbc37b64ab15419e7bb06587c3c74e54dfb10
indrasetyow <indrawibowo4567@gmail.com>
2021-03-23 13:01:52 +0700

    Menu & Floating Button

---
+++ app/src/main/java/com/example/constraint/ActivityKedua.java
@@ -0,0 +1,27 @@
+package com.example.constraint;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityKedua extends AppCompatActivity{
+    TextView txEmail, txPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kedua);
+
+        txEmail = findViewById(R.id.tvEmail);
+        txPassword = findViewById(R.id.tvPassword);
+
+        Bundle bundle = getIntent().getExtras();
+
+        String email = bundle.getString("a");
+        String pass = bundle.getString("b");
+
+        txEmail.setText(email);
+        txPassword.setText(pass);
+    }
+}
--- app/src/main/java/com/example/constraint/MainActivity.java
+++ app/src/main/java/com/example/constraint/MainActivity.java
@@ -1,14 +1,100 @@
 package com.example.constraint;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edemail, edpassword;
+    Button btnsignin;
+
+    String nama,password;
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.mnDaftar)
+        {
+            Intent i = new Intent(getApplicationContext(), DaftarActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnsignin=findViewById((R.id.signIn));
+        edemail=findViewById(R.id.Email);
+        edpassword=findViewById(R.id.Password);
+
+        btnsignin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View View) {
+                String email = "indrasetyow";
+                String pass = "123";
+
+                nama = edemail.getText().toString();
+                password = edpassword.getText().toString();
+
+                if (nama.equals(email) && password.equals(pass))
+                {
+                    Toast a = Toast.makeText(getApplicationContext(), "Login Berhasil", Toast.LENGTH_LONG);
+                    a.show();
+
+                    //membuat objek bundle
+                    Bundle b = new Bundle();
+
+                    //memasukkan value dari variabel password dengan kunci "a"
+                    // dan dimasukkan kedalam bundle
+                    b.putString("a", nama.trim());
+
+                    //memasukkan value dari variabel password dengan kunci "b"
+                    //dan dimasukkan kedalam bundle
+                    b.putString("b", password.trim());
+
+                    //membuat objek intent berpindah activity dari mainactivity ke activityhasil
+                    Intent i = new Intent(getApplicationContext(), ActivityKedua.class);
+
+                    //memasukkan bundle kedalam intent untuk dikrimkan ke ActivityHasil
+                    i.putExtras(b);
+
+                    //berpindah ke ActivityHasil
+                    startActivity(i);
+                }
+                else if (nama.equals(email) || pass.equals(password))
+                {
+                    if(nama.equals(email)) {
+                        Toast ps = Toast.makeText(getApplicationContext(),"Password Salah", Toast.LENGTH_LONG);
+                        ps.show();
+                    }
+                    else {
+                        Toast es = Toast.makeText(getApplicationContext(), "Email salah", Toast.LENGTH_LONG);
+                        es.show();
+                    }
+                }
+                else {
+                    Toast m = Toast.makeText(getApplicationContext(), "email dan password salah", Toast.LENGTH_LONG);
+                    m.show();
+                }
+            }
+
+        });
     }
+
+
 }
